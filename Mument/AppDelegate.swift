5b5228dee82d4447bd5de0cde718a4ca91ad8daa
seongchan <seongchan@edkim.co>
2019-04-15 16:17:41 +0900

    [F]Firebase

--- Mument/AppDelegate.swift
+++ Mument/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         
+        FirebaseApp.configure()
+    
 //        let rootVc = MainViewController()
         let rootVc = UINavigationController.init(rootViewController: WelcomeViewController())
         let rootTabVc = MainTabBarViewController()
